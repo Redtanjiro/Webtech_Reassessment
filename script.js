2740df9e6f8002eaec3b5bed9ddeaf6b72abbd41
Redtanjiro <neelparikh7@gmail.com>
2022-04-24 00:48:31 +0530

    added code to update all visitor profile information
    added similar code to email and name to update all other information like gender, age, dob, etc..
    
    address information is remaining. and dob is coming in incorrect format, to be fixed.
    
    used jquery selector $("#id .class").html to change content for all information

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $.ajax({
     url: 'https://randomuser.me/api/',
     dataType: 'json',
     success: function (data) {
+        console.log(data.results[0])
         createVisitorUser(data);
     }
 });
@@ -13,8 +14,30 @@ $.ajax({
 //store last name in variable lastName
 //combine first name last name using firstname+" "+lastname
 //use jquery selector $("#visitorName").html() and change html
+//do same for all other information
 function createVisitorUser(data) {
-    firstName = data.results[0].name.first;
-    lastName = data.results[0].name.last;
+    // update Name
+    var firstName = data.results[0].name.first;
+    var lastName = data.results[0].name.last;
     $("#visitorName").html(firstName + " " + lastName);
+
+    // update Email
+    var email = data.results[0].email;
+    $("#visitorEmail").html(email);
+
+    // gender
+    var gender = data.results[0].gender;
+    $("#visitorGender .infoContent").html(gender);
+
+    // age
+    var age = data.results[0].dob.age;
+    $("#visitorAge .infoContent").html(age);
+
+    // bday - to make it appear properly
+    var bday = data.results[0].dob.date;
+    $("#visitorBday .infoContent").html(bday);
+
+    // phone
+    var phone = data.results[0].phone;
+    $("#visitorPhoneno .infoContent").html(phone);
 }
