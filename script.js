b4041f280a5a5be19252f79c0405909ad4f17d56
Redtanjiro <neelparikh7@gmail.com>
2022-04-24 00:37:47 +0530

    made so that createVisitorUser updates name in html
    upon successful data recieiving, run createVisitorUser() function
    
    >store first name in variable firstName
    >store last name in variable lastName
    >combine first name last name using firstname+" "+lastname
    >use jquery selector $("#visitorName").html() and change html

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-// testing example code to see how is information recieved
+// use jquery ajax object to get data from api
+//upon successful data recieiving, run createVisitorUser() function 
 $.ajax({
     url: 'https://randomuser.me/api/',
     dataType: 'json',
     success: function (data) {
-        console.log(data);
+        createVisitorUser(data);
     }
 });
 
 //a function to create the visitor user when the page is loaded
+//store first name in variable firstName
+//store last name in variable lastName
+//combine first name last name using firstname+" "+lastname
+//use jquery selector $("#visitorName").html() and change html
 function createVisitorUser(data) {
-
+    firstName = data.results[0].name.first;
+    lastName = data.results[0].name.last;
+    $("#visitorName").html(firstName + " " + lastName);
 }
